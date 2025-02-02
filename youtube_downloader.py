8167e6fd520d066e8e283b1382b2d77f85572011
anjana732 <anjanasingh1257@gmail.com>
2025-02-02 16:54:21 +0530

    Debug Code

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,21 +1,8 @@
-# import os
-
-# video_url = input("Enter YouTube video URL: ")
-
-# # Use yt-dlp with Python
-# command = f'python3 -m yt_dlp -f bestvideo+bestaudio --merge-output-format mp4 "{video_url}"'
-
-# os.system(command)
-
-# print("Download complete!")
 
 import os
 
 video_url = input("Enter YouTube video URL: ")
-
-# Use yt-dlp with Python
-command = f'python3 -m yt_dlp -f bestvideo+bestaudio --merge-output-format mp4 --audio-quality 0 "{video_url}"'
-
+command = f'yt-dlp -f bestvideo+bestaudio --merge-output-format mp4 "{video_url}"'
 os.system(command)
 
 print("Download complete!")
