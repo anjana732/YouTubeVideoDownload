a274a5134e8aca2009ad4613529ed0fd2a28f1c6
anjana732 <anjanasingh1257@gmail.com>
2025-02-02 17:18:05 +0530

    Add code

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,8 +1,16 @@
 
+# import os
+
+# video_url = input("Enter YouTube video URL: ")
+# command = f'yt-dlp -f bestvideo+bestaudio --merge-output-format mp4 "{video_url}"'
+# os.system(command)
+
+# print("Download complete!")
+
 import os
 
 video_url = input("Enter YouTube video URL: ")
-command = f'yt-dlp -f bestvideo+bestaudio --merge-output-format mp4 "{video_url}"'
+command = f'yt-dlp -f "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]" --merge-output-format mp4 "{video_url}"'
 os.system(command)
 
 print("Download complete!")
